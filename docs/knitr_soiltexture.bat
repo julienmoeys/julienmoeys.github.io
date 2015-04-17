8f9f0b48c20d24176b0fe4ad291ba55fb156bf3f
Julien MOEYS <julienmoeys@yahoo.fr>
2015-04-17 17:00:39 +0200

    Updated R package docs

---
+++ docs/knitr_soiltexture.bat
@@ -0,0 +1,8 @@
+
+REM This is necessary because running the example soiltexture_gui() 
+REM     interactively does not work (in this case)
+
+R -e "source( 'D:/Users/julienm/Documents/_WORKS/_WEB/julienmoeys.github.io/docs/knitr_soiltexture.R' ) "
+
+pause
+
