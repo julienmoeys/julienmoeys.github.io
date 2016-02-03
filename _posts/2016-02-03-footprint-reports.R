66c94d355c841c99fb456ce92485895be5454f43
Julien Moeys <julienmoeys@yahoo.fr>
2016-02-03 23:04:21 +0100

    Add the footprint reports

---
+++ _posts/2016-02-03-footprint-reports.R
@@ -0,0 +1,32 @@
+# Linux # cd /home/${USER}/Documents/julienmoeys.info/julienmoeys.github.io && R CMD BATCH --vanilla "_posts/2016-02-03-footprint-reports.R" 
+# Win   # cd /D "D:/Users/%username%/Documents/_WORKS/_WEB/julienmoeys.github.io" & CMD BATCH --vanilla "_posts/2016-02-03-footprint-reports.R" 
+
+rm( list = ls( all = TRUE ) ) 
+
+# GENERAL OPTIONS
+if( Sys.info()[[ "sysname" ]] == "Linux" ){ 
+    setwd( sprintf( 
+        wd <- "/home/%s/Documents/julienmoeys.info/julienmoeys.github.io", 
+        Sys.info()[[ "user" ]] 
+    ) ) 
+}else{ 
+    setwd( sprintf( 
+        wd <- "D:/Users/julienm/Documents/_WORKS/_WEB/julienmoeys.github.io", 
+        Sys.info()[[ "user" ]] 
+    ) )
+}   
+
+source( "assets/R/referencesBlender.R" )
+
+# Reference list ================================================
+
+out <- referenceBlender(
+    fileIn   = "_posts/2016-02-03-footprint-reports.md0", 
+    fileOut  = "_posts/2016-02-03-footprint-reports.md", 
+    filesBib = sprintf( 
+        "assets/bib/%s", 
+        c( "FOOTPRINT_REPORTS.BIB" ) ), 
+    highlightName = "vwxy" 
+)   
+
+
